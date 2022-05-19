4f2acb1534992aeaced4afa2f64c97d8403d80a3
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2022-05-19 08:35:45 +0800

    ui

--- Information System/Form1.Designer.cs
+++ Information System/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.customControlElipse1 = new Information_System.Components.Elipse();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // customControlElipse1
@@ -36,11 +37,19 @@
             this.customControlElipse1.CornerRadius = 30;
             this.customControlElipse1.TargetControl = this;
             // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(73, 98);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1080, 720);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
@@ -51,6 +60,7 @@
         #endregion
 
         private Components.Elipse customControlElipse1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
